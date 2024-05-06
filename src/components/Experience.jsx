541a906e3b1d0747171ac66052000cd3dd306a52
snehamoybag <snehamoybag2@gmail.com>
2024-05-06 23:57:10 +0530

    feat(Experience): make Expecrience section editable

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import experience from "../data/experience";
+import initialExpData from "../data/experience";
+import EditButton from "./buttons/EditButton";
+import EditModal from "./EditModal";
 
 const Experience = () => {
-  const [experienceData, setExperienceData] = useState(experience);
+  const [experienceData, setExperienceData] = useState(initialExpData);
 
   const experienceItems = experienceData.map((experience) => {
     const { job, startYear, endYear } = experience;
@@ -17,10 +19,86 @@ const Experience = () => {
     );
   });
 
+  const [expFieldsData, setExpFieldsData] = useState(experienceData);
+
+  const handleExpFieldChange = (event, index, propertyName) => {
+    const copyOfPrevFieldsData = JSON.parse(JSON.stringify(expFieldsData));
+
+    const editedData = copyOfPrevFieldsData[index];
+    editedData[propertyName] = event.target.value;
+
+    const newFieldsData = copyOfPrevFieldsData;
+    setExpFieldsData(newFieldsData);
+  };
+
+  const editModalFields = expFieldsData.map((exp, index) => {
+    const { job, startYear, endYear } = exp;
+
+    const jobTitleId = "job_" + index;
+    const startYearId = "startYear_" + index;
+    const endYearId = "endYear_" + index;
+
+    return (
+      <fieldset key={index}>
+        <label htmlFor={jobTitleId}>
+          Job Title:
+          <input
+            type="text"
+            name={jobTitleId}
+            id={jobTitleId}
+            value={job}
+            onChange={(e) => handleExpFieldChange(e, index, "job")}
+          />
+        </label>
+        <label htmlFor={startYearId}>
+          Start Year:
+          <input
+            type="text"
+            name={startYearId}
+            id={startYearId}
+            value={startYear}
+            onChange={(e) => handleExpFieldChange(e, index, "startYear")}
+          />
+        </label>
+        <label htmlFor={endYearId}>
+          End Year:
+          <input
+            type="text"
+            name={endYearId}
+            id={endYearId}
+            value={endYear}
+            onChange={(e) => handleExpFieldChange(e, index, "endYear")}
+          />
+        </label>
+      </fieldset>
+    );
+  });
+
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const toggleEditModalState = () => setIsEditModalOpen(!isEditModalOpen);
+
+  const handleEditModalFormSubmit = () => {
+    setExperienceData(expFieldsData);
+    toggleEditModalState();
+  };
+
   return (
     <section className="experience">
-      <h2>Experience</h2>
+      <h2>
+        <span>Experience</span>
+        <EditButton handleClick={toggleEditModalState} />
+      </h2>
       <ol>{experienceItems}</ol>
+      {isEditModalOpen && (
+        <EditModal
+          id="experience-edit-modal"
+          title="Edit Experience"
+          handleFormSubmit={handleEditModalFormSubmit}
+          handleCancel={toggleEditModalState}
+        >
+          {editModalFields}
+        </EditModal>
+      )}
     </section>
   );
 };
