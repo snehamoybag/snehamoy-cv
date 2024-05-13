a814473f108d83934a6f5cb714feca914fafbadb
snehamoybag <snehamoybag2@gmail.com>
2024-05-13 22:31:16 +0530

    feat(modal styling): add edit modal styling

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -1,6 +1,7 @@
 import FormSubmitButton from "./buttons/FormSubmitButton";
 import FormCancelButton from "./buttons/FormCancelButton";
 import AddMoreButton from "./buttons/AddMoreButton";
+import "../styles/EditModal.css";
 
 const EditModal = ({
   id,
@@ -13,28 +14,30 @@ const EditModal = ({
   const isChildrenAvailable = children && children.length > 0;
 
   return (
-    <dialog id={id} className="edit-modal" open>
-      <h2>{title}</h2>
+    <>
+      <dialog id={id} className="edit-modal" open>
+        <h2 className="edit-modal__title">{title}</h2>
 
-      <form action="/">
-        {isChildrenAvailable ? (
-          children
-        ) : (
-          <p>
-            <span className="text--big">Error 404</span>: No contents to edit
-          </p>
-        )}
-      </form>
-      <div className="edit-modal__buttons-wrapper">
-        {isChildrenAvailable && (
-          <FormSubmitButton handleClick={handleFormSubmit} />
-        )}
-
-        <FormCancelButton handleClick={handleCancel} />
-
-        {handleAddMore && <AddMoreButton handleClick={handleAddMore} />}
-      </div>
-    </dialog>
+        <form action="/" className="edit-modal__form">
+          {isChildrenAvailable ? (
+            children
+          ) : (
+            <p>
+              <span className="text--big">Error 404</span>: No contents to edit
+            </p>
+          )}
+        </form>
+        <div className="edit-modal__buttons-wrapper">
+          {isChildrenAvailable && (
+            <FormSubmitButton handleClick={handleFormSubmit} />
+          )}
+          <FormCancelButton handleClick={handleCancel} />
+          {handleAddMore && <AddMoreButton handleClick={handleAddMore} />}
+        </div>
+      </dialog>
+      {/* custom styled backdrop since inbuild dialog::backdrop only works with showModal() function */}
+      <div className="edit-modal__backdrop"></div>
+    </>
   );
 };
 
